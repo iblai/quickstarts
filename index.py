528a5d46baae5fefea751bcd009d53258b7adcac
godwin <ojaragodwin@gmail.com>
2024-07-18 23:21:41 +0300

    add doc to index web urls

--- index.py
+++ index.py
@@ -1,3 +1,5 @@
+import json
+
 import requests
 
 # Manager base url to be changed
@@ -10,11 +12,12 @@ ACCESS_TOKEN = ""  # this must be a tenant api key
 
 # THIS ENDPOINT ALLOWS ONLY TENANT ADMINS TO CREATE MENTORS.
 
-def index_document():
+
+def index_document_from_file():
     """
-    Index document for a particular mentor.
+    Index file document for a particular mentor.
     """
-    pathway = "", # The pathway here should match the name of the mentor which the document is being indexed
+    pathway = ""  # The pathway here should match the name of the mentor for which the document is being indexed
     payload = {"type": "file", "pathway": pathway}
     files = [
         (
@@ -33,7 +36,37 @@ def index_document():
         files=files,
         headers=headers,
     )
-    
+
+    if response.ok:
+        print(response.json())
+
+    else:
+        print(response.status_code)
+        print(response.text)
+
+
+def index_document_from_url():
+    """
+    Index url document for a particular mentor.
+
+    """
+    headers = {
+        "Authorization": f"Api-Token {ACCESS_TOKEN}",
+        "Content-Type": "application/json",
+    }
+    pathway = ""  # The pathway here should match the name of the mentor for which the document is being indexed
+    url = "" # url of the webpage you want indexed.
+    data = {
+        "type": "webcrawler",
+        "pathway": pathway,
+        "url": url,
+    }
+    response = requests.post(
+        f"{BASE_URL}api/ai-index/orgs/{TENANT}/users/{USERNAME}/documents/train/",
+        headers=headers,
+        data=json.dumps(data),
+    )
+
     if response.ok:
         print(response.json())
 
